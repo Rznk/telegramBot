f5ae279bb262f99865531c8b3d8e2217ccc42738
Andrew Reznik <rezyadlf@gmail.com>
2018-05-24 15:11:20 +0300

    Added publishing and bot shipnextIco

--- controller/publish.js
+++ controller/publish.js
@@ -1,13 +1,12 @@
 const token = require("../config/accs").token;
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(token, {polling: false});
-
+const chatId = -260151687;
 
 let publish = async (message)=>{
-
-
+    let result = await bot.sendMessage(chatId, message, {parse_mode: "Markdown"});
+    return !!(result && result.message_id)
 };
 
 
 exports.publish = publish;
-
--- routes/publish.js
+++ routes/publish.js
@@ -1,11 +1,11 @@
-let publishDomain = require("../controller/publish").publish;
+let publishCtrl = require("../controller/publish").publish;
 
 
 
 let publishMessage = async(req, res)=>{
   let msg = req.body.text;
-  let results = await publishDomain(msg);
-  res.json("1");
+  let result = await publishCtrl(msg);
+  res.json(result);
 };
 
 exports.publishMessage = publishMessage;
