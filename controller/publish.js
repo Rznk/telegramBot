6eb150796cdcd036fdaf185787ca8037ef26c143
Andrew Reznik <rezyadlf@gmail.com>
2018-05-24 15:31:44 +0300

    changed group to channel

--- controller/publish.js
+++ controller/publish.js
@@ -1,14 +1,14 @@
 const token = require("../config/accs").token;
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(token, {polling: false});
-const chatId = -260151687;
+const chatId = -1001196056682;
 
 let publish = async (message)=>{
     try {
         let result = await bot.sendMessage(chatId, message, {parse_mode: "Markdown"});
         return !!(result && result.message_id)
     } catch (e) {
-        console.log(`{E} in controller/publish:: ${e}`);
+        console.error(`{E} in controller/publish:: ${e}`);
         return e
     }
 };
